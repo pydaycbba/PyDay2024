e937967de8228d0a5cc7e95a9828f83889caa599
PyDayCochabamba <pydaycbba@gmail.com>
2024-05-03 19:17:26 -0400

    No guardar

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -7,28 +7,28 @@ import Contact from './sections/Contact';
 
 
 
-const Home = () => {
-  return (
-    <div>
-      <div className='mainContainer'>
-        <Navbar />
-        <section id='Home'>
-          <div className='contentLogo'>
-            <img className="logo_inicio" src={Images.pydayCochabamba} alt='pyday logo'/>
-            <img className="CommingSoon" src={Images.comingSoon} alt="CommingSoon" />
-            <p className="button">15 de Junio</p>
-          </div>
-          <div className='contentBody'>
-            <p>Muy pronto tendremos nuestro primer PyDay en Cochabamba</p>
-            <p>Un evento donde se reunirá la comunidad para aprender y celebrar un día completo en torno a Python, uno de los lenguajes de programación másversátil y usado en la industria, ¡disfrutando de diferentes ponencias y talleres!</p>
-          </div>
-        </section>
-      </div>
-      <CodeOfConduct />
-      <Contact />
-      <Footer />
-    </div>
-  )
-}
+	const Home = () => {
+	return (
+		<div>
+		<div className='mainContainer'>
+			<Navbar />
+			<section id='Home'>
+			<div className='contentLogo'>
+				<img className="logo_inicio" src={Images.pydayCochabamba} alt='pyday logo'/>
+				<img className="CommingSoon" src={Images.comingSoon} alt="CommingSoon" />
+				<p className="button">15 de Junio</p>
+			</div>
+			<div className='contentBody'>
+				<p>Muy pronto tendremos nuestro primer PyDay en Cochabamba</p>
+				<p>Un evento donde se reunirá la comunidad para aprender y celebrar un día completo en torno a Python, uno de los lenguajes de programación másversátil y usado en la industria, ¡disfrutando de diferentes ponencias y talleres!</p>
+			</div>
+			</section>
+		</div>
+		<CodeOfConduct />
+		<Contact />
+		<Footer />
+		</div>
+	)
+	}
 
 export default Home;
