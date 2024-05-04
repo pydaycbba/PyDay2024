a8ec5dcbccdef3de15a6a4ff1749e4ca1039f4a7
JHOSEPH-CACERES <jhoseph@localhost.localdomain>
2024-05-04 18:16:31 -0400

    add footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,50 +1,37 @@
-import React from 'react'
-import Icons from '../../assets/icons';
-import Images from '../../assets/images';
+import React from "react";
+import Icons from "../../assets/icons";
+import RRSS from "../../constants";
+import Images from "../../assets/images";
 
-import './footer.css';
+import "./footer.css";
 
 const Footer = () => {
   return (
     <footer class="footer">
-        <div class="container-logo">
-            <div class="cont1">
-                <img class="logo1" src={Images.logoPyCbba} alt="" />
-                <div class="container_column_contactofooter">
-                    <a href="https://www.facebook.com/pythoncbba" target="_blank">
-                        <img src={Icons.logoFacebook} alt="" />
-                        <span>pythoncbba</span>
-                    </a>
-                    <a href="https://www.instagram.com/pythoncbba/" target="_blank">
-                        <img src={Icons.logoInstagram} alt="" />
-                        <span>@pythoncbba</span>
-                    </a>
-                    <a href="mailto: pythoncochabamba@gmail.com" target="_blank">
-                        <img src={Icons.logoEmail} alt="" />
-                        <span>pythoncochabamba@gmail.com</span>
-                    </a>
-                </div>
-            </div>
-            <div class="cont1">
-                <img class="logo2" src={Images.logoPyladies} alt="" />
-                <div class="container_column_contactofooter">
-                    <a href="https://www.facebook.com/pyladiescbba" target="_blank">
-                        <img src={Icons.logoFacebook} alt="" />
-                        <span>pyladiescbba</span>
-                    </a>
-                    <a href="https://www.instagram.com/pyladiescbba/" target="_blank">
-                        <img src={Icons.logoInstagram} alt="" />
-                        <span>@pyladiescbba</span>
-                    </a>
-                    <a href="mailto: cochabamba@pyladies.com" target="_blank">
-                        <img src={Icons.logoEmail} alt="" />
-                        <span>cochabamba@pyladies.com</span>
-                    </a>
-                </div>
-            </div>
+      <div className="footer-content">
+        <div className="title">
+          <h1>Contáctanos</h1>
         </div>
+        <div className="cont">
+          <img src={Images.pydayCbbaOriginal} alt="PyDay Cbba" />
+          <div className="cont-icon">
+            <a href={RRSS.facebook.url} target="_blank">
+              <img src={Icons.logoFacebook} alt="Facebook" />
+              <span>{RRSS.facebook.laber}</span>
+            </a>
+            <a href={RRSS.instagram.url} target="_blank">
+              <img src={Icons.logoInstagram} alt="Instagram" />
+              <span>{RRSS.instagram.laber}</span>
+            </a>
+            <a href={RRSS.email.url} target="_blank">
+              <img src={Icons.logoEmail} alt="Email" />
+              <span>{RRSS.email.laber}</span>
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
